26680eac1af796bdd6bdad5ee3afd729fc0d7ce4
Gaks3 <dwi66116@gmail.com>
2024-12-22 00:31:08 +0700

    feat: add accessibility button

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,22 +1,25 @@
-"use client";
+'use client'
 
-import { Button } from "@/components/ui/button";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-import { useTheme } from "next-themes";
+import { Button } from '@/components/ui/button'
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons'
+import { useTheme } from 'next-themes'
 
 export function ModeToggle() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme()
 
   return (
     <Button
-      variant="ghost"
-      type="button"
-      size="icon"
-      className="px-2"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      variant='ghost'
+      type='button'
+      size='icon'
+      className='px-2'
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      name='mode-toggle'
+      role='button'
+      aria-label='mode-toggle'
     >
-      <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
-      <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
+      <SunIcon className='h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200' />
+      <MoonIcon className='hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200' />
     </Button>
-  );
+  )
 }
