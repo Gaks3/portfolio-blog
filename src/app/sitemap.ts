799ef3ff5b9847346479fc67a3c491fe479d3c96
Gaks3 <dwi66116@gmail.com>
2024-12-21 18:59:00 +0700

    feat: create dynamic sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,16 @@
+import type { MetadataRoute } from 'next'
+import { DATA } from '@/data/resume'
+import { getBlogPosts } from '@/data/blog'
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = DATA.url
+
+  const blogs = await getBlogPosts()
+
+  return [
+    {
+      url: baseUrl,
+    },
+    ...blogs.map((blog) => ({ url: blog.slug })),
+  ]
+}
