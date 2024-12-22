0f3afc40ebd0e5d7d4c320e02f14251b37765be5
Gaks3 <dwi66116@gmail.com>
2024-12-22 17:34:26 +0700

    feat: add search and pagination function

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -10,6 +10,7 @@ import { transformerCopyButton } from '@rehype-pretty/transformers'
 import withSlugs from 'rehype-slug'
 import withToc from '@stefanprobst/rehype-extract-toc'
 import { notFound } from 'next/navigation'
+import { DATA } from './resume'
 
 type Metadata = {
   title: string
@@ -18,8 +19,34 @@ type Metadata = {
   image: string
 }
 
-function getMDXFiles(dir: string) {
-  return fs.readdirSync(dir).filter((file) => path.extname(file) === '.mdx')
+function getMDXFiles(
+  dir: string,
+  page: number = 1,
+  search: string | undefined
+) {
+  let files = fs
+    .readdirSync(dir)
+    .filter((file) => path.extname(file) === '.mdx')
+
+  if (search) {
+    files = files.filter((name) => name.includes(search))
+  }
+
+  files = files
+    .map((file) => ({
+      name: file,
+      time: fs.statSync(`${dir}/${file}`).birthtime.getTime(),
+    }))
+    .sort((a, b) => b.time - a.time)
+    .map((file) => file.name)
+
+  const startIndex = (page - 1) * DATA.blogsPerPage
+  const endIndex = startIndex + DATA.blogsPerPage
+
+  return {
+    files: files.slice(startIndex, endIndex),
+    total: files.length,
+  }
 }
 
 export async function markdownToHTML(markdown: string) {
@@ -67,21 +94,31 @@ export async function getPost(slug: string) {
   }
 }
 
-async function getAllPosts(dir: string) {
-  let mdxFiles = getMDXFiles(dir)
-  return Promise.all(
-    mdxFiles.map(async (file) => {
-      let slug = path.basename(file, path.extname(file))
-      let { metadata, source } = await getPost(slug)
-      return {
-        metadata,
-        slug,
-        source,
-      }
-    })
-  )
+async function getAllPosts(
+  dir: string,
+  page: number = 1,
+  search: string | undefined
+) {
+  let { files, total } = getMDXFiles(dir, page, search)
+  return {
+    files: await Promise.all(
+      files.map(async (file) => {
+        let slug = path.basename(file, path.extname(file))
+        let { metadata, source } = await getPost(slug)
+        return {
+          metadata,
+          slug,
+          source,
+        }
+      })
+    ),
+    total,
+  }
 }
 
-export async function getBlogPosts() {
-  return getAllPosts(path.join(process.cwd(), 'content'))
+export async function getBlogPosts(
+  page: number = 1,
+  search: string | undefined = undefined
+) {
+  return getAllPosts(path.join(process.cwd(), 'content'), page, search)
 }
